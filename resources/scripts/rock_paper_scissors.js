f32c381848c25bf8a022e78e3adb83a71923bbbb
1DELETED9 <moutian@mail.uc.edu>
2024-11-16 18:14:11 -0500

    first update

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -1,38 +1,38 @@
-class RockPaperScissors {
-  constructor(username) {
-    this.username = username;
-         this.score = {
-      user: 0,
-      cpu: 0 
-    },
-    this.gameHistoryLog = [];
+
+  export class RockPaperScissors {
+      constructor(username) {
+          this.username = username;
+      }
+
+  generateCPUResponse() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
   }
 
-  /**
-   * RETURN: one of the following values (`rock`, `paper`, `scissors`)
-   * using Math.random() method, you should be able to get one of the following values
-   */
   generateCPUResponse(){
       var acceptedValues = [ `rock`, `paper`, `scissors` ];
     const randomIndexNumber = Math.floor(Math.random() * 3); 
 
     return acceptedValues[randomIndexNumber];
   }
-  /**
-   * returns one of the following values: `win`, `lose`, `tie`
-   * tie:
-   *     the user selection the same as the CPU
-   * win: 
-   *    (user is `rock` and cpu is `scissors
-   *     OR
-   *    (user is `paper` and cpu is `rock`) 
-   *     OR 
-   *    (user is `scissors` and cpu is `paper`)
-   * `lose`:
-   *    the opposite case :)
-   * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   */
+
+  determineWinner(userSelection, cpuSelection) {
+    if (userSelection === cpuSelection) {
+      return 'tie'; 
+    }
+    
+    if (
+      (userSelection === 'rock' && cpuSelection === 'scissors') ||
+      (userSelection === 'paper' && cpuSelection === 'rock') ||
+      (userSelection === 'scissors' && cpuSelection === 'paper')
+    ) {
+      return 'win'; 
+    }
+    
+      return 'lose'; 
+    }
+  
   determineWinner(userSelection, cpuSelection){
     if (userSelection == cpuSelection) { 
          return 'tie'; 
@@ -49,10 +49,13 @@ class RockPaperScissors {
     }
   }
 
-  /**
-   * 
-   * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   */
+  play(userSelection) {
+    const validChoices = ['rock', 'paper', 'scissors'];
+    if (!validChoices.includes(userSelection)) {
+      throw new Error(`Invalid selection: ${userSelection}. Please choose 'rock', 'paper', or 'scissors'.`);
+    }
+  }
+
   play(userSelection){
     const cpuSelection = this.generateCPUResponse(); 
     const results = this.determineWinner(userSelection, cpuSelection); 
